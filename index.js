1d011e7a383b6aefae11cbd263ca5e2641bd6ec6
JokerLS <38219751+JokerLS@users.noreply.github.com>
2018-07-04 16:23:55 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+
+http.createServer(function(req,res) {
+    var pathObj = url.parse(req.url, true)
+    console.log(pathObj)
+
+    switch(pathObj.pathname) {
+        case '/getWeather':
+            var ret
+            if(pathObj.query.city == 'guangzhou') {
+                ret = {
+                    city: 'guangzhou',
+                    weather: 'Sunny'
+                }
+            }else {
+                ret = {
+                    city: pathObj.query.city,
+                    weather: 'unknow'
+                }
+            }
+            res.end(JSON.stringify(ret))
+            break;
+        case '/user/Lan':
+            res.end(fs.readFileSync(__dirname + '/sample/test.html'))
+            break;
+        default:
+            res.end(fs.readFileSync(__dirname + '/sample' + pathObj.pathname))
+    }
+}).listen(8060)
